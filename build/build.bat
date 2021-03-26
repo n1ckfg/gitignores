2fa98d8a3f26689b224b29d73b204f89a48979f1
Nick Fox-Gieg <nick@fox-gieg.com>
2021-03-26 18:25:00 -0400

    shell

--- build/build.bat
+++ build/build.bat
@@ -5,32 +5,32 @@ cd ..
 
 copy gitattributes.txt openframeworks\.gitattributes
 copy openframeworks_gitignore.txt openframeworks\.gitignore
-copy gitattributes-lfs.txt openframeworks-lfs\.gitattributes
+copy gitattributes-LFS.txt openframeworks-lfs\.gitattributes
 copy openframeworks_gitignore.txt openframeworks-lfs\.gitignore
 
 copy gitattributes.txt p5js\.gitattributes
 copy p5js_gitignore.txt p5js\.gitignore
-copy gitattributes-lfs.txt p5js-lfs\.gitattributes
+copy gitattributes-LFS.txt p5js-lfs\.gitattributes
 copy p5js_gitignore.txt p5js-lfs\.gitignore
 
 copy gitattributes.txt unity\.gitattributes
 copy unity_gitignore.txt unity\.gitignore
-copy gitattributes-lfs.txt unity-lfs\.gitattributes
+copy unity_gitattributes-LFS.txt unity-lfs\.gitattributes
 copy unity_gitignore.txt unity-lfs\.gitignore
 
 copy gitattributes.txt unreal\.gitattributes
 copy unreal_gitignore.txt unreal\.gitignore
-copy gitattributes-lfs.txt unreal-lfs\.gitattributes
+copy unreal_gitattributes-LFS.txt unreal-lfs\.gitattributes
 copy unreal_gitignore.txt unreal-lfs\.gitignore
 
 copy gitattributes.txt processing\.gitattributes
 copy processing_gitignore.txt processing\.gitignore
-copy gitattributes-lfs.txt processing-lfs\.gitattributes
+copy gitattributes-LFS.txt processing-lfs\.gitattributes
 copy processing_gitignore.txt processing-lfs\.gitignore
 
 copy gitattributes.txt processingpy\.gitattributes
 copy processingpy_gitignore.txt processingpy\.gitignore
-copy gitattributes-lfs.txt processingpy-lfs\.gitattributes
+copy gitattributes-LFS.txt processingpy-lfs\.gitattributes
 copy processingpy_gitignore.txt processingpy-lfs\.gitignore
 
 @pause
--- build/build.command
+++ build/build.command
@@ -13,30 +13,30 @@ cd ..
 
 cp gitattributes.txt openframeworks/.gitattributes
 cp openframeworks_gitignore.txt openframeworks/.gitignore
-cp gitattributes-lfs.txt openframeworks-lfs/.gitattributes
+cp gitattributes-LFS.txt openframeworks-lfs/.gitattributes
 cp openframeworks_gitignore.txt openframeworks-lfs/.gitignore
 
 cp gitattributes.txt p5js/.gitattributes
 cp p5js_gitignore.txt p5js/.gitignore
-cp gitattributes-lfs.txt p5js-lfs/.gitattributes
+cp gitattributes-LFS.txt p5js-lfs/.gitattributes
 cp p5js_gitignore.txt p5js-lfs/.gitignore
 
 cp gitattributes.txt unity/.gitattributes
 cp unity_gitignore.txt unity/.gitignore
-cp gitattributes-lfs.txt unity-lfs/.gitattributes
+cp unity_gitattributes-LFS.txt unity-lfs/.gitattributes
 cp unity_gitignore.txt unity-lfs/.gitignore
 
 cp gitattributes.txt unreal/.gitattributes
 cp unreal_gitignore.txt unreal/.gitignore
-cp gitattributes-lfs.txt unreal-lfs/.gitattributes
+cp unreal_gitattributes-LFS.txt unreal-lfs/.gitattributes
 cp unreal_gitignore.txt unreal-lfs/.gitignore
 
 cp gitattributes.txt processing/.gitattributes
 cp processing_gitignore.txt processing/.gitignore
-cp gitattributes-lfs.txt processing-lfs/.gitattributes
+cp gitattributes-LFS.txt processing-lfs/.gitattributes
 cp processing_gitignore.txt processing-lfs/.gitignore
 
 cp gitattributes.txt processingpy/.gitattributes
 cp processingpy_gitignore.txt processingpy/.gitignore
-cp gitattributes-lfs.txt processingpy-lfs/.gitattributes
+cp gitattributes-LFS.txt processingpy-lfs/.gitattributes
 cp processingpy_gitignore.txt processingpy-lfs/.gitignore
